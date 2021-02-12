07a93b507803ac24dddd192674f440bac9c7ce02
gunisettyharini <77732231+gunisettyharini@users.noreply.github.com>
2021-02-12 20:13:26 +0530

    Update Line_Comparision_Problem.java

--- Line_Comparision_Problem.java
+++ Line_Comparision_Problem.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
-public class LineComparison
+public class EmpWage
 {
 	public static void main(String[] args)
 	{
 	    System.out.println("Calculate Length Between Two Point");
 	    int x1,x2,y1,y2;
-	    System.out.println("Calculate Length Between Two Point");
-	    int x1,x2,y1,y2;
-            double Length;
+            double l1;
             Scanner sc=new Scanner(System.in);
             System.out.println("enter x1=");
-            Length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-            System.out.println("Length Between"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+Length);
+            x1=sc.nextInt();
+            System.out.println("enter x2 =");
+            x2=sc.nextInt();
+            System.out.println("enter y1 =");
+            y1=sc.nextInt();
+            System.out.println("enter y2 =");
+            y2=sc.nextInt();
+            l1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+            System.out.println("Length Between"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+l1);
 	    int p1,p2,q1,q2;
-	    double Length2;
-	    double Length2;
+	    double l2;
             System.out.println("enter p1 =");
             p1=sc.nextInt();
             System.out.println("enter p2 =");
@@ -23,11 +27,11 @@ public class LineComparison
             q1=sc.nextInt();
             System.out.println("enter q2 =");
             q2=sc.nextInt();
-            Length2=Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));
-            System.out.println("Length between"+"("+p1+","+q1+"),"+"("+p2+","+q2+")="+Length2);
-            double obj1=new double(Length1);
-            double obj2=new double(Length2);
-            if(obj1==obj2))
+            l2=Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));
+            System.out.println("Length between"+"("+p1+","+q1+"),"+"("+p2+","+q2+")="+l2);
+            double obj1=(l1);
+            double obj2=(l2);
+            if(obj1==obj2)
             {
         	System.out.println("Both lines are equal");
             } 
